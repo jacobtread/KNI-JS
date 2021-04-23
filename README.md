8e7f0174046b8b8db8bb10fcb0990f662d933859
Jacob <jacobtread@gmail.com>
2021-04-23 23:18:14 +1200

    Updated code and replaced standard js with typescript

--- README.md
+++ README.md
@@ -1,32 +1,35 @@
 # KAMAR Notices Interface
-Node/JavaScript Edition
+
+Node/JavaScript/Typescript Edition
 
 KNI (KAMAR Notices Interface) is a project designed to bring a way of accessing notices from
-the [KAMAR](https://kamar.nz) portal software. KNIs goal is to produce usable libraries in as many
-languages as possible
+the [KAMAR](https://kamar.nz) portal software. KNIs goal is to produce usable libraries in as many languages as possible
 
 [![NPM](https://nodei.co/npm/kni-js.png)](https://nodei.co/npm/kni-js/)
 
-KNI is available on NPM 
+KNI is available on NPM
+
 ```console
 $ npm i kni-js
 ```
 
-
+You can provide Date objects, strings or null providing null or no value will use the current date
 
 ### Retrieving Notices
-```javascript
-const KNI = require('kni-js')
 
-const kni = new KNI(host = 'demo.school.kiwi');
-kni.retrieve('01/01/2020').then(response => {
-    const notices = response.notices;
-    for (let i = 0; i < notices.length; i++) {
-        console.log(notices[i]);
-    }
-}).catch(error => {
-    console.error(error)
-})
+```javascript
+const KNI = require('kni-js');
+
+const kni = new KNI('portal.your.school.nz'); // Your school portal domain (or https://portal.your.school.nz)
+kni.get(
+    new Date() /* You can use date objects, string dates, and null (strings must be dd/MM/yyyy e.g 21/12/2021) */)
+    .then(response => {
+        const generalNotices = response.general; // Get the general notices
+        const meetingNotices = response.meetings; // Get the meeting notices
+    })
+    .catch(err => { // If notices couldn't be retrieved
+        console.error('Failed to retrieve notices: ' + err);
+    })
 ```
 
 By Jacobtread
--- index.ts
+++ index.ts
@@ -1,16 +1,39 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+import {AxiosError, AxiosRequestConfig, AxiosResponse} from "axios";
+
 const axios = require('axios');
 // We need format and parse from date-fns for parsing and formatting dates
-const { format, parse } = require('date-fns');
+const {format, parse} = require('date-fns');
 // We need
 const parser = require('fast-xml-parser');
+
 const KAMAR_CONSTANTS = {
     key: 'vtku',
     userAgent: 'KAMAR/ Linux/ Android/'
 };
+
+interface Notice {
+    level: string | 'All' | 'Juniors' | 'Seniors';
+    subject: string;
+    body: string;
+    teacher: string;
+}
+
+interface MeetingNotice extends Notice {
+    place: string;
+    date: Date | null;
+    time: string;
+}
+
+interface KNIResponse {
+    general: Notice[];
+    meetings: MeetingNotice[];
+    all: Array<Notice | MeetingNotice>;
+}
+
 class KNI {
-    constructor(host, useHTTPS = true) {
+    url: string
+
+    constructor(host: string, useHTTPS: boolean = true) {
         if (!host.match(/^http(s)?:\/\/.*$/)) { // Check if the host starts with https:// or http://
             host = (useHTTPS ? 'https' : 'http') + '://' + host; // If not we should append it
         }
@@ -20,31 +43,30 @@ class KNI {
         host += 'api/api.php'; // Appending the api file to the url
         this.url = host; // Set the url
     }
-    get(inDate = null) {
+
+    get(inDate: string | Date | null = null): Promise<KNIResponse> {
         return new Promise((resolve, reject) => {
-            const inFormat = 'dd/MM/yyyy';
-            let date;
+            const inFormat: string = 'dd/MM/yyyy';
+            let date: string;
             if (inDate == null) {
                 date = format(new Date(), inFormat); // Format the current date
-            }
-            else {
+            } else {
                 if (typeof inDate == 'string') {
                     date = inDate; // Use the provided string
-                }
-                else {
+                } else {
                     date = format(inDate, inFormat); // Format the provided date object
                 }
             }
-            const params = new URLSearchParams();
+            const params: URLSearchParams = new URLSearchParams();
             params.append('Key', KAMAR_CONSTANTS.key); // Use the default authentication key
             params.append('Command', 'GetNotices'); // The KAMAR command for notices
             params.append('ShowAll', 'YES'); // Usage Unknown but provided in app
             params.append('Date', date.toString()); // The provided date
-            const config = {
+            const config: AxiosRequestConfig = {
                 url: this.url,
-                method: 'POST',
+                method: 'POST', // Request must be over POST
                 headers: {
-                    'User-Agent': 'KAMAR/ Linux/ Android/',
+                    'User-Agent': 'KAMAR/ Linux/ Android/', // KAMAR Requires a special User-Agent
                     'Content-Type': 'application/x-www-form-urlencoded' // KAMAR requires x-www-form-urlencoded content
                 },
                 data: params,
@@ -55,24 +77,22 @@ class KNI {
                     }
                     return parser.parse(data); // Parse the xml object to js object
                 }
-            };
-            axios.request(config).then((response) => {
+            }
+            axios.request(config).then((response: AxiosResponse) => {
                 if (response.status != 200) {
-                    reject(`Unable to connect to KAMAR responded with: ${response.status} ${response.statusText}`);
-                }
-                else {
+                    reject(`Unable to connect to KAMAR responded with: ${response.status} ${response.statusText}`)
+                } else {
                     if (!response.data.hasOwnProperty("NoticesResults")) {
-                        reject('Invalid response from KAMAR');
-                    }
-                    else {
-                        const result = response.data.NoticesResults; // The main response xml object
-                        const general = []; // The general notices array
-                        const meetings = []; // The meeting notices array
+                        reject('Invalid response from KAMAR')
+                    } else {
+                        const result: any = response.data.NoticesResults; // The main response xml object
+                        const general: Notice[] = []; // The general notices array
+                        const meetings: MeetingNotice[] = []; // The meeting notices array
                         if (result.hasOwnProperty("GeneralNotices")) { // Make sure the xml has general notices
                             const generalNotices = result.GeneralNotices; // Get the general notices object
                             if (generalNotices.hasOwnProperty("General")) { // Make sure there is notice children
                                 const kGeneral = generalNotices.General; // Get the notice children
-                                kGeneral.forEach((notice) => {
+                                kGeneral.forEach((notice: any) => { // Loop through all the notices
                                     if (notice.hasOwnProperty("Level") // Make sure the notice has a level
                                         && notice.hasOwnProperty("Subject") // Make sure the notice has a subject
                                         && notice.hasOwnProperty("Body") // Make sure the notice has a body
@@ -93,7 +113,7 @@ class KNI {
                             const meetingNotices = result.MeetingNotices; // Get the meeting notices object
                             if (meetingNotices.hasOwnProperty("Meeting")) { // Make sure there is notice children
                                 const kMeetings = meetingNotices.Meeting; // Get the notice children
-                                kMeetings.forEach((notice) => {
+                                kMeetings.forEach((notice: any) => { // Loop through all the notices
                                     if (notice.hasOwnProperty("Level") // Make sure the notice has a level
                                         && notice.hasOwnProperty("Subject") // Make sure the notice has a subject
                                         && notice.hasOwnProperty("Body") // Make sure the notice has a body
@@ -104,11 +124,10 @@ class KNI {
                                     ) {
                                         // We can parse the date because its a constant format unlike the time and place
                                         const dateMeet = notice.DateMeet; // Get the notice time
-                                        let date; // Placeholder for the date
+                                        let date: Date | null; // Placeholder for the date
                                         if (dateMeet == null || dateMeet.length < 1) { // If there is no date
                                             date = null; // Set date to null
-                                        }
-                                        else {
+                                        } else {
                                             date = parse(dateMeet, 'yyyy-MM-dd', new Date()); // Parse the date
                                         }
                                         // Push the notice to the meeting notices
@@ -126,13 +145,14 @@ class KNI {
                             }
                         }
                         const all = general.concat(meetings); // Create the all notices array by joining the two arrays
-                        resolve({ general, meetings, all }); // Resolve with the data
+                        resolve({general, meetings, all}); // Resolve with the data
                     }
                 }
-            }).catch((err) => {
+            }).catch((err: AxiosError) => {
                 reject(err.message); // Reject because the request failed
             });
         });
     }
 }
+
 module.exports = KNI; // Export the KNI class
